cb7f57df912f08ca7df0e3e6ebc7c589e9b62471
Ahmed Helmy <helmyahmed248@gmail>
2020-11-11 17:17:35 +0200

    finished employee

--- resources/views/admin/hygieneInspections/form.blade.php
+++ resources/views/admin/hygieneInspections/form.blade.php
@@ -12,26 +12,39 @@
   </div>
  @endsection
 @section('content')
- <div class="row">
+<div class="row">
 
-<div class="col-md-12 col-sm-12 ">
-    <div class="x_panel">
-      <div class="x_title">
+    <div class="col-md-12 col-sm-12 ">
+        <div class="card">
+            <div class="card-header">
+                Add New Data
+            </div>
+            <div class="card-body">
+                <div class="col-12">
+                    <div class="mt-4 d-flex justify-content-between">
+                        <div class="col form-group">
+                            <label for="title">العنوان</label>
+                            <input type="text" id="title" name="title" value="" class="form-control " placeholder="العنوان" autocomplete="off" required="">
+                            <small id="emailHelp" class="form-text text-danger">هذا الحقل مطلوب</small>
+                        </div>
+                        <div class="col form-group">
+                            <label for="gender">Gender</label>
+                            <select class="form-control @error('gender') is-invalid @enderror" required id="gender" name="gender">
+                                <option value="">Choose..</option>
+                                <option @if(isset($employee) && $employee->gender =='male') selected @endif value="male">Male</option>
+                                <option @if(isset($employee) && $employee->gender =='female') selected @endif value="female">Female</option>
 
-        <h2>Add New Data <small>Custom design</small></h2>
-
-        <div class="clearfix"></div>
-      </div>
-
-      <div class="x_content">
-
-
-
-
-
-
-      </div>
+                            </select>
+                            @error('gender')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
- </div>
+</div>
   @endsection
